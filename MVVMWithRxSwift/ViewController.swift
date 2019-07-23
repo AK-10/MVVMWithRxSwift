756c28608c344525f8983d59eec8f9b9f76b772c
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-07-24 04:34:49 +0900

    impl view

--- MVVMWithRxSwift/ViewController.swift
+++ MVVMWithRxSwift/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextFiled: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
