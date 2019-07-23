152d255c0d6ea4c233c29ea7014767172fb93360
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-07-24 05:09:00 +0900

    impl model

---
+++ MVVMWithRxSwift/Model.swift
@@ -0,0 +1,44 @@
+//
+//  Model.swift
+//  MVVMWithRxSwift
+//
+//  Created by Atsushi KONISHI on 2019/07/24.
+//  Copyright © 2019 小西篤志. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+enum ModelError: Error {
+    case invalidId
+    case invalidPassword
+    case invalidIdAndPassword
+}
+
+protocol ModelProtocol {
+    func validate(idText: Optional<String>, passwordText: Optional<String>) -> Observable<Void>
+}
+
+class Model: ModelProtocol {
+    func validate(idText: Optional<String>, passwordText: Optional<String>) -> Observable<Void> {
+        switch (idText, passwordText) {
+        case (.none, .none):
+            return Observable.error(ModelProtocol.invalidIdAndPassword)
+        case (.none, .some):
+            return Observable.error(ModelProtocol.invalidId)
+        case (.some, .none):
+            return Observable.error(ModelProtocol.invalidPassword)
+        case (let idText?, let passwordText?)
+            switch (idText.isEmpty, passwordText?.idEmpty) {
+            case (true, true):
+                return Observable.error(ModelError.invalidIdAndPassword)
+            case (false, false):
+                return Observable.just(())
+            case (true, false):
+                return Observable.error(ModelError.invalidId)
+            case (false, true):
+                return Observable.error(ModelError.invalidPassword)
+            }
+        }
+    }
+}
